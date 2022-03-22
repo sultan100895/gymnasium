89271f009bae5f06be66c8069f6db35d25561b63
SULTAN <utsho.edu2014@gmail.com>
2022-03-23 00:08:38 +0600

    responsive completed

---
+++ js/custom.js
@@ -0,0 +1,118 @@
+$(function(){
+  //for slider
+  $('.banner-slider').slick({
+    arrows:false, //arrow remove korar jonno
+    dots:true,
+    autoplay:true,
+    fade: true,
+    autoplaySpeed: 3000,
+  });
+
+  // about popup video
+    $('.venobox').venobox();
+
+  // team slider
+  $('.team-slider').slick({
+    arrows: false,
+    slidesToShow: 4,
+  });
+
+  // testimonial-slider
+  $('.testimonial-slider').slick({
+    arrows: false,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay:true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: false,
+          autoplay: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: false,
+          arrows: false,
+          autoplay: true
+
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          arrows: false,
+          autoplay: true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+  // counter up
+  $('.counter').counterUp({
+    delay:10,
+    time:1000
+  });
+
+  //brand-slider
+  $('.brand-slider').slick({
+    slidesToShow: 5,
+    prevArrow: '<i class="fas fa-chevron-left left-arrow"></i>',
+    nextArrow: '<i class="fas fa-chevron-right right-arrow"></i>',
+    centerMode: true,
+    centerPadding: '0px',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: false,
+          autoplay: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: false,
+          arrows: false,
+          autoplay: true
+
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          arrows: false,
+          autoplay: true
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
